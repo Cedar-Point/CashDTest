fd17612194945d14a705d46d93e83061a56638ce
Dylan K. Bickerstaff <dylan.k.bickerstaff@ad.cedarfair.com>
2019-05-31 12:25:04 -0400

    Add project files.

---
+++ CashDTest/Main.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace CashDTest
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPop = new System.Windows.Forms.Button();
+            this.StatusStrip = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblCDStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnPop
+            // 
+            this.btnPop.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnPop.Location = new System.Drawing.Point(13, 12);
+            this.btnPop.Name = "btnPop";
+            this.btnPop.Size = new System.Drawing.Size(204, 81);
+            this.btnPop.TabIndex = 0;
+            this.btnPop.Text = "Pop";
+            this.btnPop.UseVisualStyleBackColor = true;
+            this.btnPop.Click += new System.EventHandler(this.BtnPop_Click);
+            // 
+            // StatusStrip
+            // 
+            this.StatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus,
+            this.lblCDStatus});
+            this.StatusStrip.Location = new System.Drawing.Point(0, 108);
+            this.StatusStrip.Name = "StatusStrip";
+            this.StatusStrip.Size = new System.Drawing.Size(229, 22);
+            this.StatusStrip.SizingGrip = false;
+            this.StatusStrip.TabIndex = 1;
+            this.StatusStrip.Text = "Status";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(45, 17);
+            this.lblStatus.Text = "Status: ";
+            // 
+            // lblCDStatus
+            // 
+            this.lblCDStatus.Name = "lblCDStatus";
+            this.lblCDStatus.Size = new System.Drawing.Size(58, 17);
+            this.lblCDStatus.Text = "Unknown";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 130);
+            this.Controls.Add(this.StatusStrip);
+            this.Controls.Add(this.btnPop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cash Drawer Test";
+            this.TopMost = true;
+            this.StatusStrip.ResumeLayout(false);
+            this.StatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.StatusStrip StatusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.ToolStripStatusLabel lblCDStatus;
+    }
+}
+
---
+++ CashDTest/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CashDTest
+{
+    public partial class Main : Form
+    {
+        private CashDrawerAPI CashDrawer = new CashDrawerAPI();
+        public Main()
+        {
+            InitializeComponent();
+            CashDrawer.CashDrawerStatusChanged += CashDrawer_CashDrawerStatusChanged;
+        }
+        private void CashDrawer_CashDrawerStatusChanged(CashDrawerStatus cashDrawerStatus)
+        {
+            if(cashDrawerStatus == CashDrawerStatus.OPEN)
+            {
+                lblCDStatus.Text = "Open";
+            }
+            if (cashDrawerStatus == CashDrawerStatus.CLOSED)
+            {
+                lblCDStatus.Text = "Closed";
+            }
+        }
+        private void BtnPop_Click(object sender, EventArgs e)
+        {
+            CashDrawer.OpenCashDrawer();
+        }
+    }
+}
