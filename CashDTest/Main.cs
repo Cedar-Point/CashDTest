2c744de2ae30b49470c0dadf13e6f217ef311a01
Dylan K. Bickerstaff <dylan@belowaverage.org>
2019-06-09 15:18:15 -0400

    Update

--- CashDTest/Main.cs
+++ CashDTest/Main.cs
@@ -1,30 +1,36 @@
 ﻿using System;
 using System.Windows.Forms;
+using MicrosCashDrawer;
 
 namespace CashDTest
 {
     public partial class Main : Form
     {
-        private CashDrawerAPI CashDrawer = new CashDrawerAPI();
+        private CashDrawer CashDrawer = new CashDrawer();
         public Main()
         {
             InitializeComponent();
+            UpdateStatus();
             CashDrawer.CashDrawerStatusChanged += CashDrawer_CashDrawerStatusChanged;
         }
         private void CashDrawer_CashDrawerStatusChanged(CashDrawerStatus cashDrawerStatus)
         {
-            if(cashDrawerStatus == CashDrawerStatus.OPEN)
-            {
-                lblCDStatus.Text = "Open";
-            }
-            if (cashDrawerStatus == CashDrawerStatus.CLOSED)
-            {
-                lblCDStatus.Text = "Closed";
-            }
+            UpdateStatus();
         }
         private void BtnPop_Click(object sender, EventArgs e)
         {
             CashDrawer.OpenCashDrawer();
+        }
+        private void UpdateStatus()
+        {
+            if (CashDrawer.CashDrawerStatus == CashDrawerStatus.OPEN)
+            {
+                lblCDStatus.Text = "Open";
+            }
+            if (CashDrawer.CashDrawerStatus == CashDrawerStatus.CLOSED)
+            {
+                lblCDStatus.Text = "Closed";
+            }
         }
     }
 }
